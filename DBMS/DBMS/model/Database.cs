7140ceb7ac53816348008afc0fe673bc220e9b4b
Fekete Norbert <fsim1315@scs.ubbcluj.ro>
2016-10-23 21:59:39 +0300

    no message

--- DBMS/DBMS/model/Database.cs
+++ DBMS/DBMS/model/Database.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DBMS.backend
+namespace DBMS.model
 {
-    class Table
+    class Database
     {
-        private String name;
-
-
     }
 }
