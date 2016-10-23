77973aa0e436e5376d99a0465c1b1111466ba7a5
Fekete Norbert <fsim1315@scs.ubbcluj.ro>
2016-10-23 21:40:10 +0300

    model started

---
+++ DBMS/DBMS/backend/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMS.backend
+{
+   
+    class Database
+    {
+        private String name;
+        private List<Table> tabels;
+
+        public Database(String name)
+        {
+            this.name = name;
+        }
+
+    }
+}
