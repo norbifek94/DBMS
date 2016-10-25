bf8166d30dc591793214589148d8f6f3c2242361
Fekete Norbert <fsim1315@scs.ubbcluj.ro>
2016-10-25 18:13:05 +0300

    ooops

---
+++ DBMS/DBMS/src/Server.java
@@ -0,0 +1,14 @@
+
+public class Server {
+
+	public static void main(String[] args) {
+		Database db = new Database("D://database.xml");
+		//db.createXMLFile();
+		//db.createDatabase("test4");
+		//db.dropDatabase("test4");
+		//db.creatTable("test", "testTable");
+		//db.creatAttribute("testTable", "testAttr", "char", "64", "0");
+		db.dropTable("testTable");
+	}
+
+}
